d678909d89182db6262015f4e276667d9cc090cc
Techa <112974952+techarb2@users.noreply.github.com>
2022-10-14 22:27:51 -0500

    Update index.js

--- script/index.js
+++ script/index.js
@@ -36,7 +36,7 @@ app.destroyParticles = function() {
 document.querySelector('#open-nav').addEventListener('click', app.openNav);
 document.querySelector('#close-nav').addEventListener('click', app.closeNav);
 
-document.title = localStorage.getItem('incog||title') || 'Incognito';
+document.title = localStorage.getItem('incog||title') || 'Ingrain Network';
 window.icon = document.querySelector('#favicon');
 
 icon.href = localStorage.getItem('incog||icon') || './index.svg';
